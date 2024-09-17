9d382524f37d19af0d694d9f89cd99285f151e4b
Lily <133771406+lilylilo@users.noreply.github.com>
2024-09-17 19:01:45 -0300

    Create indice.py

---
+++ indice.py
@@ -0,0 +1,11 @@
+INDICE = 13
+SOMA = 0
+K = 0
+
+# Loop que funciona como "Enquanto K < INDICE"
+while K < INDICE:
+    K += 1  # Incrementa K
+    SOMA += K  # Adiciona K à soma
+
+# Exibe o valor final de SOMA
+print("SOMA =", SOMA)
